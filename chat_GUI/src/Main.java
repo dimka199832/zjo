224ea9ecd86e6908f5094127b874d55174b8217e
Pavel Gass <pavelgass@MacBook.local>
2017-03-18 20:22:09 +0100

    Update

--- chat_GUI/src/Main.java
+++ chat_GUI/src/Main.java
@@ -1,9 +1,9 @@
-package Education;
-
+/**
+ * Created by pavelgass on 18.03.17.
+ */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        
+        //
     }
 }
